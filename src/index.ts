7f901962096b04c926e483292ddd7d7994e9728c
Pedro <9307951+PedroS11@users.noreply.github.com>
2024-01-12 21:03:25 +0000

    Poll epiode every 6h

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { pollEpisode } from "./infrastructure/bot/service";
 import { schedule } from "node-cron";
 
-schedule("0 */1 * * *", async () => {
+schedule("0 */6 * * *", async () => {
     // send the message here
     await pollEpisode();
 });
