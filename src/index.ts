06f44ca5f0e6b4f580e23a6da9515fd602491a73
Pedro <9307951+PedroS11@users.noreply.github.com>
2024-01-16 20:14:21 +0000

    Use nodecron with newest version

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import "dotenv/config";
 import { pollEpisode } from "./infrastructure/bot/service";
 import { AppDataSource } from "./infrastructure/dabtabase/dataSource";
+import { schedule } from "node-cron";
 
-// schedule("0 */6 * * *", async () => {
-//     // send the message here
-//     await pollEpisode();
-// });
-
-(async () => {
+schedule("0 */6 * * *", async () => {
     await AppDataSource.initialize();
+
     await pollEpisode();
-})();
+});
+
+// (async () => {
+//     await AppDataSource.initialize();
+//     await pollEpisode();
+// })();
