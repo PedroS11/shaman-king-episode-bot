a5b1ec0e63c8bfd2ba84544dd404595c45ec0687
Pedro <9307951+PedroS11@users.noreply.github.com>
2024-03-01 10:14:47 +0000

    Update baseUrl

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const SHAMAN_KING_FLOWERS_API =
-	"https://aniwatch.to/ajax/v2/episode/list/18826";
+	"https://hianime.to/ajax/v2/episode/list/18826";
 export const MAX_NUMBER_OF_EPISODES = 13;
