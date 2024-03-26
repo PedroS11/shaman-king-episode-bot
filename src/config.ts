5c5717fb666a92343d0c6d8e0d8344b6c8ec7030
Pedro <9307951+PedroS11@users.noreply.github.com>
2024-03-26 18:09:45 +0000

    Fix 403 url

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const SHAMAN_KING_FLOWERS_API =
-	"https://aniwave.to/ajax/episode/list/15390?vrf=HJ5foHkkpT1YElj_";
+	"https://aniwave.to/ajax/episode/list/15390?vrf=R%5CW0SVE-Zn%3E%3F";
 export const MAX_NUMBER_OF_EPISODES = 13;
 export const WIKIPEDIA_URL =
 	"https://en.wikipedia.org/wiki/Shaman_King:_Flowers";
